b7e593ff8e2a04c8761a9ac99a144f0573f9b227
Nian0bussou <nianlegare@outlook.com>
2024-11-15 20:08:20 -0500

    reformat flags file

--- cmd/flags/flags.go
+++ cmd/flags/flags.go
@@ -10,9 +10,21 @@ type Values struct {
 
 func GetFlags() (val Values) {
 
-	flag.BoolVar(&val.Only_local, "ol", true, "only allow local ip")
-	flag.StringVar(&val.Gallery_path, "galpath", "images_paths.json", "specify the path of the json to the images paths for gallery")
-	flag.IntVar(&val.Port, "port", 8080, "use specific IP port")
+	flag.BoolVar(&val.Only_local,
+		"ol",
+		true,
+		"only allow local ip",
+	)
+	flag.StringVar(&val.Gallery_path,
+		"galpath",
+		"images_paths.json",
+		"specify the path of the json to the images paths for gallery",
+	)
+	flag.IntVar(&val.Port,
+		"port",
+		8080,
+		"use specific IP port",
+	)
 
 	flag.Parse()
 	return val
