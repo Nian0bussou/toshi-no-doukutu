42c8e3922747e0dc444c8d13ce080153651703af
Nian0bussou <nianlegare@outlook.com>
2024-11-15 21:34:24 -0500

    started resume page

--- cmd/resume/resume.go
+++ cmd/resume/resume.go
@@ -10,9 +10,9 @@ import (
 
 func Resume() {
 	fs := http.FileServer(http.Dir("./resume"))
-	http.HandleFunc("/resume", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/resume/", func(w http.ResponseWriter, r *http.Request) {
 		ip.GetIP(r)
-		fmt.Println(gtime.GetTime(), "INFO: home; accessed")
+		fmt.Println(gtime.GetTime(), "INFO: resume; accessed")
 		http.StripPrefix("/resume", fs).ServeHTTP(w, r)
 	})
 }
